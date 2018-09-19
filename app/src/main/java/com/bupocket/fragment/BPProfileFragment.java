651cacc8a1545c71ec68bb7457c2bf5aa89208d3
胡楠 <137035119@qq.com>
2018-09-19 17:13:20 +0800

    update profile

--- app/src/main/java/com/bupocket/fragment/BPProfileFragment.java
+++ app/src/main/java/com/bupocket/fragment/BPProfileFragment.java
@@ -35,14 +35,10 @@ public class BPProfileFragment extends BaseFragment{
     private String currentAccAddress;
     private String currentAccNick;
 
-    @BindView(R.id.showMyAddressLv)
-    LinearLayout mShowMyaddressL;
     @BindString(R.string.qr_copy_success_message)
     String copySuccessMessage;
     @BindView(R.id.userNick)
     TextView userNickTx;
-    @BindView(R.id.userBcAddress)
-    TextView addressTv;
     @BindView(R.id.changePwdRL)
     RelativeLayout mChangePwdRL;
     @BindView(R.id.helpFeedbackRL)
@@ -57,8 +53,7 @@ public class BPProfileFragment extends BaseFragment{
     protected View onCreateView() {
         View root = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_profile, null);
         ButterKnife.bind(this, root);
-        initData(root);
-        showMyAddress(currentAccAddress);
+        initData();
         mChangePwdRL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,64 +75,15 @@ public class BPProfileFragment extends BaseFragment{
         return root;
     }
 
-    private void initData(View view){
+    private void initData(){
         sharedPreferencesHelper = new SharedPreferencesHelper(getContext(), "buPocket");
         currentAccNick = sharedPreferencesHelper.getSharedPreference("currentAccNick", "").toString();
-        currentAccAddress = sharedPreferencesHelper.getSharedPreference("currentAccAddr", "").toString();
 
         userNickTx.setText(currentAccNick);
 
-        String hideCurrentAccAddress = AddressUtil.anonymous(currentAccAddress);
-        addressTv.setText(hideCurrentAccAddress);
         mVersionNameTv.setText(CommonUtil.packageName(getContext()));
     }
 
-    private void showMyAddress(final String currentAccAddress) {
-        mShowMyaddressL.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final QMUIBottomSheet sheet = new QMUIBottomSheet(getContext());
-                sheet.setContentView(R.layout.show_address_layout);
-
-
-                TextView addressTxt = sheet.findViewById(R.id.printAccAddressTv);
-                addressTxt.setText(currentAccAddress);
-
-                Bitmap mBitmap = QRCodeUtil.createQRCodeBitmap(currentAccAddress, 356, 356);
-                ImageView mImageView = sheet.findViewById(R.id.qr_pocket_address_image);
-                mImageView.setImageBitmap(mBitmap);
-
-                sheet.findViewById(R.id.addressCopyBtn).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        ClipboardManager cm = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
-                        ClipData mClipData = ClipData.newPlainText("Label", currentAccAddress);
-                        cm.setPrimaryClip(mClipData);
-                        final QMUITipDialog copySuccessDiglog = new QMUITipDialog.Builder(getContext())
-                                .setIconType(QMUITipDialog.Builder.ICON_TYPE_SUCCESS)
-                                .setTipWord(copySuccessMessage)
-                                .create();
-                        copySuccessDiglog.show();
-                        getView().postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                copySuccessDiglog.dismiss();
-                            }
-                        }, 1500);
-                    }
-                });
-
-                sheet.findViewById(R.id.closeBtn).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        sheet.dismiss();
-                    }
-                });
-                sheet.show();
-
-            }
-        });
-    }
 
     private void gotoChangePwdFragment(){
         startFragment(new BPChangePwdFragment());
