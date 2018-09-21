3665b9414182d70f672d918c124e4514a6b5efa1
胡楠 <137035119@qq.com>
2018-09-21 16:23:03 +0800

    update some icon

--- app/src/main/java/com/bupocket/fragment/home/HomeFragment.java
+++ app/src/main/java/com/bupocket/fragment/home/HomeFragment.java
@@ -15,7 +15,6 @@ import com.bupocket.R;
 import com.bupocket.base.BaseFragment;
 import com.bupocket.fragment.BPAssetsFragment;
 import com.bupocket.fragment.BPProfileFragment;
-import com.bupocket.fragment.otc.OtcMainFragment;
 import com.qmuiteam.qmui.widget.QMUIPagerAdapter;
 import com.qmuiteam.qmui.widget.QMUITabSegment;
 
