f8363c320cdf5c2a96b2c0ec1c912640dd2d3318
zhangxinars <zhangxinars@163.com>
2018-10-19 14:03:36 +0800

    fix min fee

--- app/src/main/java/com/bupocket/common/Constants.java
+++ app/src/main/java/com/bupocket/common/Constants.java
@@ -2,18 +2,18 @@ package com.bupocket.common;
 
 public class Constants {
     //开发环境
-    public static final String WEB_SERVER_DOMAIN = "http://52.80.218.114:8081";
-    public static final String BUMO_NODE_URL = "http://seed1.bumotest.io:26002";
+//    public static final String WEB_SERVER_DOMAIN = "http://52.80.218.114:8081";
+//    public static final String BUMO_NODE_URL = "http://seed1.bumotest.io:26002";
     //测试环境
-//    public static final String WEB_SERVER_DOMAIN = "http://api-bp.bumotest.io/";
-//    public static final String BUMO_NODE_URL = "http://wallet-node.bumotest.io";
+    public static final String WEB_SERVER_DOMAIN = "http://api-bp.bumotest.io/";
+    public static final String BUMO_NODE_URL = "http://wallet-node.bumotest.io";
     //生产环境
 //    public static final String WEB_SERVER_DOMAIN = "https://api-bp.bumo.io/";
 //    public static final String BUMO_NODE_URL = "http://wallet-node.bumo.io";
     public static final Integer SEND_TOKEN_NOTE_MAX_LENGTH = 20;
     public static final Integer HELP_FEEDBACK_CONTENT_LENGTH = 100;
     public static final Integer HELP_FEEDBACK_CONTACT_LENGTH = 20;
-    public static final double MIN_FEE = 0.003;
+    public static final double MIN_FEE = 0.005;
     public static final double RESERVE_AMOUNT = 0.02;
     public static final Double ACTIVE_AMOUNT_FEE = 0.02;
     public static final double MIN_SEND_AMOUNT = 0.00000001;
