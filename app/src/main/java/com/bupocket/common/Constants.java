dcf460322b70b99507c9c81d8660d82e4fcde884
zhangxinars <zhangxinars@163.com>
2018-11-07 16:04:49 +0800

    switch URL

--- app/src/main/java/com/bupocket/common/Constants.java
+++ app/src/main/java/com/bupocket/common/Constants.java
@@ -2,15 +2,17 @@ package com.bupocket.common;
 
 public class Constants {
     //开发环境
-    public static final String WEB_SERVER_DOMAIN = "http://52.80.218.114:8081/";
-    public static final String BUMO_NODE_URL = "http://seed1.bumotest.io:26002";
-    public static final String PUSH_MESSAGE_SOCKET_URL = "http://192.168.6.98:3100/";
+//    public static final String WEB_SERVER_DOMAIN = "http://52.80.218.114:8081/";
+//    public static final String BUMO_NODE_URL = "http://seed1.bumotest.io:26002";
+//    public static final String PUSH_MESSAGE_SOCKET_URL = "http://192.168.6.98:3100/";
     //测试环境
-//    public static final String WEB_SERVER_DOMAIN = "http://api-bp.bumotest.io/";
-//    public static final String BUMO_NODE_URL = "http://wallet-node.bumotest.io";
+    public static final String WEB_SERVER_DOMAIN = "http://api-bp.bumotest.io/";
+    public static final String BUMO_NODE_URL = "http://wallet-node.bumotest.io";
+    public static final String PUSH_MESSAGE_SOCKET_URL = "https://ws-tools.bumotest.io";
     //生产环境
 //    public static final String WEB_SERVER_DOMAIN = "https://api-bp.bumo.io/";
 //    public static final String BUMO_NODE_URL = "http://wallet-node.bumo.io";
+//    public static final String PUSH_MESSAGE_SOCKET_URL = "https://ws-tools.bumo.io";
     public static final Integer SEND_TOKEN_NOTE_MAX_LENGTH = 20;
     public static final Integer HELP_FEEDBACK_CONTENT_LENGTH = 100;
     public static final Integer HELP_FEEDBACK_CONTACT_LENGTH = 20;
