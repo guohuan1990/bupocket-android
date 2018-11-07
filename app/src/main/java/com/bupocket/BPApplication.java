98f4cb83896141533026e041699555e26b0360bf
zhangxinars <zhangxinars@163.com>
2018-11-07 13:27:54 +0800

    fix https socket.io

--- app/src/main/java/com/bupocket/BPApplication.java
+++ app/src/main/java/com/bupocket/BPApplication.java
@@ -11,9 +11,19 @@ import com.bupocket.utils.LocaleUtil;
 import com.squareup.leakcanary.LeakCanary;
 
 import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import okhttp3.OkHttpClient;
 
 public class BPApplication extends Application {
     @SuppressLint("StaticFieldLeak")
@@ -24,12 +34,57 @@ public class BPApplication extends Application {
     }
 
     private Socket mSocket;
+
     {
         try {
+            String socketUrl = Constants.PUSH_MESSAGE_SOCKET_URL;
+            HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+
+                }
+
+                @Override
+                public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+
+                }
+
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return new java.security.cert.X509Certificate[0];
+                }
+            }};
+            X509TrustManager trustManager = (X509TrustManager) trustAllCerts[0];
+
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, null);
+            SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .hostnameVerifier(hostnameVerifier)
+                    .sslSocketFactory(sslSocketFactory, trustManager)
+                    .build();
+
+            IO.Options opts = new IO.Options();
+            opts.callFactory = okHttpClient;
+            opts.webSocketFactory = okHttpClient;
+            mSocket = IO.socket(socketUrl, opts);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+        /*try {
             mSocket = IO.socket(Constants.PUSH_MESSAGE_SOCKET_URL);
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
-        }
+        }*/
     }
 
     public Socket getSocket() {
