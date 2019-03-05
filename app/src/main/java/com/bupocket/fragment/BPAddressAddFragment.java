a8eb3ceffd94270cc5b689642418cb13302f79b1
zhangxinars <zhangxinars@163.com>
2019-03-05 18:16:33 +0800

    optimize

--- app/src/main/java/com/bupocket/fragment/BPAddressAddFragment.java
+++ app/src/main/java/com/bupocket/fragment/BPAddressAddFragment.java
@@ -200,7 +200,6 @@ public class BPAddressAddFragment extends BaseFragment {
         intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
         intentIntegrator.setPrompt(getResources().getString(R.string.wallet_scan_notice));
         intentIntegrator.setCaptureActivity(CaptureActivity.class);
-        // 开始扫描
         intentIntegrator.initiateScan();
     }
 
